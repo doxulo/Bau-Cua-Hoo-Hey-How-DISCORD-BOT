9aed900c5d074a13305814616679e2eecfca39bb
dxl-1805 <72246173+dxl-1805@users.noreply.github.com>
2022-04-04 23:00:48 -0500

    minor changes to display bot info

--- cogs/About.py
+++ cogs/About.py
@@ -14,27 +14,26 @@ class About(commands.Cog):
 
     @commands.command(name="info", aliases=[])
     async def info(self, ctx):
-        """ Displays information about the bot."""
-        message = discord.Embed(title="Info", description=f"{self.bot.description}", color=0xdc1d24)
-        message.add_field(name="Author", value=f"{await self.bot.fetch_user(self.bot.owner_id)}")
-        message.add_field(name="Version", value="1.0.0")
+        """ Hiển thị thông tin Bot."""
+        message = discord.Embed(title="Thông tin", description=f"{self.bot.description}", color=0xdc1d24)
+        message.add_field(name="Tác giả", value=f"{await self.bot.fetch_user(self.bot.owner_id)}")
+        message.add_field(name="Phiên bản", value="1.0.0")
         message.add_field(name="Prefix", value="!")
-        message.add_field(name="Source Code", value="<blank>")
-        message.add_field(name="GitHub", value="<blank>")
+        message.add_field(name="Source Code", value="https://github.com/dxl-1805/Bau-Cua-Hoo-Hey-How-DISCORD-BOT")
+        message.add_field(name="GitHub", value="https://github.com/dxl-1805")
         message.add_field(name="Discord", value="<blank>")
-        message.add_field(name="Support Server", value="<blank>")
-        message.add_field(name="Invite", value="<blank>")
-        message.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+        message.set_footer(text=f"Yêu cầu bởi {ctx.author}", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=message)
     
     @commands.command(name="author", aliases=[])
     async def author(self, ctx):
+        """ Hiển thị thông tin tác giả."""
         author = await self.bot.fetch_user(self.bot.owner_id)
-        message = discord.Embed(title="Author", description=f"{author}", color=0xdc1d24)
+        message = discord.Embed(title="Tác giả", description=f"{author}", color=0xdc1d24)
         message.set_thumbnail(url=author.avatar_url)
         message.add_field(name="ID", value=f"{author.id}")
         message.add_field(name="Facebook", value=f"{misc.author_facebook()}")
-        message.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+        message.set_footer(text=f"Yêu cầu bởi {ctx.author}", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=message)
 
 def setup(bot):
